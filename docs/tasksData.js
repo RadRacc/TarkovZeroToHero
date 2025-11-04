3dc8ed740bb320cebd4c950fe0f875915dbb81b8
RadRacc <141393685+RadRacc@users.noreply.github.com>
2025-11-04 10:02:59 -0800

    Update tasksData.js

--- docs/tasksData.js
+++ docs/tasksData.js
@@ -16,7 +16,8 @@ const TASKS_DATA = [
         ],
         rewards: [
             { type: "roubles", amount: 5000 },
-            { type: "item", name: "3 Bolts", icon: "icon-bolts.png" }
+            { type: "item", name: "2 Bolts", icon: "icon-bolts.png" },
+            { type: "item", name: "1 Weapon Parts", icon: "icon-weapon-parts.png }
         ]
     },
     {
@@ -25,7 +26,7 @@ const TASKS_DATA = [
         title: "Emergency Repairs",
         objectiveSummary: "Repair 2 generators on Factory located at Medical Tent.",
         map: "Factory",
-        requirements: ["Target Practice", { type: "item", name: "2 Toolset", icon: "icon-toolset.png" }],
+        requirements: ["Target Practice", "2 Toolset"],
         initial_equipment: [],
         walkthrough: "The first generator can be located inside the Medical Tent in the right corner. The second is just outside to the left next to a pillar.",
         dialogueInitial: "Listen, I've got a problem over on Factory. Not my problem, mind you, but I need it fixed. Those stupid generators near the med tent are offline, and I had some... equipment running off that grid. It's a simple job: you need to go there and get two of those things repaired. I don't care if you have to slap them with a wrench or yell at them, just make them work. It's a busy spot, I know, but you need to get used to the noise. Get in, fix the power, and get out. Don't come back without that juice flowing, or you'll be fixing my mood next.",
